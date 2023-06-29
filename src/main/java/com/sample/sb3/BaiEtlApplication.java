b7526eb0cd790e475bf68312dccb03fa6813806f
lngo0 <longngo.gm@gmail.com>
2023-06-29 10:43:06 -0500

    Initial commit

---
+++ src/main/java/com/sample/sb3/BaiEtlApplication.java
@@ -0,0 +1,13 @@
+package com.sample.sb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiEtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaiEtlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/sb3/BaiEtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.sb3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiEtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
