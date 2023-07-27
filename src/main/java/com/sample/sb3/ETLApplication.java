9d3c4a888fd9d95a2a09ea9373ebfd2bda07582d
lngo0 <longngo.gm@gmail.com>
2023-07-27 08:57:28 -0500

    Rename main app

--- src/main/java/com/sample/sb3/ETLApplication.java
+++ src/main/java/com/sample/sb3/ETLApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BaiEtlApplication {
+public class ETLApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BaiEtlApplication.class, args);
+		SpringApplication.run(ETLApplication.class, args);
 	}
 
 }
