730d3d245f8b91cdb6264e723b219f8bb312e68a
lngo0 <longngo.gm@gmail.com>
2023-08-31 14:20:31 -0500

    Adding Security config

---
+++ src/main/java/com/sample/sb3/config/SecurityConfiguration.java
@@ -0,0 +1,61 @@
+package com.sample.sb3.config;
+
+import org.apache.catalina.core.ApplicationFilterChain;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration  {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+    @Bean
+    public SecurityFilterAutoConfiguration securityFilterAutoConfiguration() {
+        return  new SecurityFilterAutoConfiguration();
+    }
+
+    @Bean
+    public ApplicationFilterChain applicationFilterChain() {
+        return  new ApplicationFilterChain();
+    }
+
+
+    @Bean
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+
+    }
+
+}
